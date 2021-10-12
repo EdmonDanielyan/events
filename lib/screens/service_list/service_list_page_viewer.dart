70a013603870b0fe5f486d5a36bf89d7351f86af
ibragim CHekaev <ibragimchekaev@MacBook-Pro-ibragim.local>
2021-10-12 09:03:54 +0300

    Wrapped service list with will pop widget

--- lib/screens/service_list/service_list_page_viewer.dart
+++ lib/screens/service_list/service_list_page_viewer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ink_mobile/components/app_bars/back_btn.dart';
 import 'package:ink_mobile/components/app_bars/ink_app_bar_with_text.dart';
+import 'package:ink_mobile/components/new_bottom_nav_bar/cubit/new_bottom_nav_bar_cubit.dart';
 import 'package:ink_mobile/localization/localization_cubit/localization_cubit.dart';
 import 'package:ink_mobile/localization/strings/language.dart';
 import 'package:ink_mobile/models/page_viewer.dart';
@@ -25,41 +26,68 @@ class ServiceListPageViewerState extends State<ServiceListPageViewer> {
   Widget build(BuildContext context) {
     _strings = BlocProvider.of<LocalizationCubit>(context, listen: true).state;
     return Scaffold(
-      body: PageView(
-        controller: pageViewer.pageController,
-        children: [
-          ServiceListScreen(),
-          feedBackScreen(),
-          referencesScreen(),
-          medicalInsuranceScreen(),
-        ],
-        onPageChanged: pageViewer.onPageChanged,
-        physics: NeverScrollableScrollPhysics(),
+      body: BlocListener<NewBottomNavBarCubit, int>(
+        listener: (context, state) {
+          print(state);
+        },
+        child: PageView(
+          controller: pageViewer.pageController,
+          children: [
+            ServiceListScreen(),
+            feedBackScreen(),
+            referencesScreen(),
+            medicalInsuranceScreen(),
+          ],
+          onPageChanged: pageViewer.onPageChanged,
+          physics: NeverScrollableScrollPhysics(),
+        ),
       ),
     );
   }
 
   Widget feedBackScreen() {
-    return FeedBackScreen(
-      appBar: appBar(_strings.feedback),
-      bottomNavigationBar: SizedBox(),
+    return wrapWithWillPop(
+      child: FeedBackScreen(
+        appBar: appBar(_strings.feedback),
+        bottomNavigationBar: SizedBox(),
+      ),
     );
   }
 
   Widget referencesScreen() {
-    return ReferencesScreen(appBar: appBar(_strings.orderInquiry));
+    return wrapWithWillPop(
+      child: ReferencesScreen(
+        appBar: appBar(_strings.orderInquiry),
+      ),
+    );
   }
 
   Widget medicalInsuranceScreen() {
-    return MedicalInsuranceScreen(appBar: appBar(_strings.getMedicalInsurance));
+    return wrapWithWillPop(
+      child: MedicalInsuranceScreen(
+        appBar: appBar(_strings.getMedicalInsurance),
+      ),
+    );
   }
 
   PreferredSizeWidget appBar(String title) {
     return InkAppBarWithText(
       title: title,
       leading: AppBarBackBtn(
-        onPressed: () => pageViewer.pageController.jumpToPage(0),
+        onPressed: () => jumpToMainPage(),
       ),
     );
   }
+
+  Widget wrapWithWillPop({required Widget child}) {
+    return WillPopScope(
+      onWillPop: () async {
+        jumpToMainPage();
+        return Future.value(false);
+      },
+      child: child,
+    );
+  }
+
+  void jumpToMainPage() => pageViewer.pageController.jumpToPage(0);
 }
