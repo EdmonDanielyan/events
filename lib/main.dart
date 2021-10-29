009e7594128c1a94f93bb07fb114338ce2efa9d1
evgeny.isaev <evgeny.isaev@latoken.com>
2021-10-29 12:36:51 +0400

    added: onMessageFuture argument for subscribeToChannel

--- lib/main.dart
+++ lib/main.dart
@@ -37,9 +37,6 @@ void main() async {
     };
     runApp(InkMobile(onAppStart: () async {
       NatsProvider natsProvider = sl<NatsProvider>();
-      natsProvider.onMessage = (channel, message) async {
-        print("onMessage.channel: $channel, message: $message");
-      };
       return await natsProvider.load();
     }));
   }, (Object error, StackTrace stack) {
