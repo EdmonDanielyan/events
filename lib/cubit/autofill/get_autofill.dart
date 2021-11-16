1e02f2f97cb470abac5a19ca22841aa6c9eb49b1
ibragim CHekaev <ibragimchekaev@MacBook-Pro-ibragim.local>
2021-11-16 19:10:50 +0300

    added fixes from master

---
+++ lib/cubit/autofill/get_autofill.dart
@@ -0,0 +1,27 @@
+import 'package:ink_mobile/core/errors/dio_error_handler.dart';
+import 'package:ink_mobile/cubit/references/sources/get_autofill_data/network.dart';
+import 'package:ink_mobile/localization/i18n/i18n.dart';
+import 'package:ink_mobile/models/autofill.dart';
+import 'package:ink_mobile/models/error_model.dart';
+import 'package:ink_mobile/models/token.dart';
+import 'package:ink_mobile/extensions/reference_autofill.dart';
+import 'package:dio/dio.dart';
+
+import '../../setup.dart';
+
+class GetAutofill {
+  Autofill autofill = Autofill();
+
+  Future<void> load() async {
+    try {
+      await Token.setNewTokensIfExpired();
+      final response = await sl<GetAutofillNetworkRequest>()();
+      autofill = response.mapResponse();
+    } on DioError catch (e) {
+      ErrorModel error = DioErrorHandler(e: e).call();
+      throw error.exception;
+    } on Exception catch (_) {
+      throw FormatException(localizationInstance.unknownError);
+    }
+  }
+}
--- lib/functions/launch_url.dart
+++ lib/functions/launch_url.dart
@@ -2,6 +2,17 @@ import 'package:ink_mobile/functions/errors.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> launchUrl(String url) async {
+  if (!url.contains("http")) {
+    final separate = url.split(":");
+    if (separate.length == 2) {
+      print(separate[0]);
+      print(separate[1]);
+      url = Uri(scheme: separate[0], path: separate[1]).toString();
+      launch(url.toString());
+    }
+  }
+
+  print(url);
   await canLaunch(url)
       ? await launch(url)
       : showErrorDialog('Не удалось перейти по внешней ссылке');
