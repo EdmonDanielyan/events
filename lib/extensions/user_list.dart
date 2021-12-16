9b97ac108e2afb3cd4e8cabec4c582bd0df72465
Ибрагим Чекаев <i.chekaev@qsoft.ru>
2021-12-16 17:35:03 +0300

    synced

--- lib/extensions/user_list.dart
+++ lib/extensions/user_list.dart
@@ -2,7 +2,7 @@ import 'package:ink_mobile/models/chat/database/chat_db.dart';
 
 extension ListUserTableExtension on List<UserTable> {
   bool compareLight(List<UserTable> list) {
-    if (this.length != list.length) return false;
+    if (this.isEmpty || this.length != list.length) return false;
 
     final firstEqual = this.first.id == list.first.id;
     final lastEqual = this.last.id == list.last.id;
