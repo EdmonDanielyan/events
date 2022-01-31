e05f342826af7c9d5df72a9452d424bb3d2a42ed
mormih <mormih@gmail.com>
2022-01-31 16:36:47 +0400

    db_enum.g.dart

---
+++ lib/messenger/models/chat/database/tables/db_enum.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'db_enum.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+const _$StoredMessageTypeEnumMap = {
+  StoredMessageType.NOT_DEFINED: 0,
+  StoredMessageType.TEXT: 1,
+  StoredMessageType.USER_LEFT: 2,
+  StoredMessageType.USER_JOINED: 3,
+  StoredMessageType.DOCUMENT: 4,
+};
+
+const _$MessageSentStatusEnumMap = {
+  MessageSentStatus.EMPTY: 0,
+  MessageSentStatus.SENDING: 1,
+  MessageSentStatus.SENT: 2,
+  MessageSentStatus.ERROR: 3,
+};
+
+const _$MessageActionsEnumMap = {
+  MessageActions.EMPTY: 0,
+  MessageActions.FORWARDED: 1,
+  MessageActions.EDITED: 2,
+  MessageActions.DELETED: 3,
+};
