120b304bb7a12d7744472ad79ef3b0ee22627251
mormih <mormih@gmail.com>
2022-02-08 20:15:46 +0400

    Добавил логику для антидулирования

--- lib/messenger/models/chat/nats/requests/create_chat_request.g.dart
+++ lib/messenger/models/chat/nats/requests/create_chat_request.g.dart
@@ -10,10 +10,21 @@ CreateChatRequest _$CreateChatRequestFromJson(Map<String, dynamic> json) =>
     CreateChatRequest(
       ownerId: json['owner_id'] as String,
       token: json['token'] as String,
+      participantId: json['participant_id'] as String?,
     );
 
-Map<String, dynamic> _$CreateChatRequestToJson(CreateChatRequest instance) =>
-    <String, dynamic>{
-      'owner_id': instance.ownerId,
-      'token': instance.token,
-    };
+Map<String, dynamic> _$CreateChatRequestToJson(CreateChatRequest instance) {
+  final val = <String, dynamic>{
+    'owner_id': instance.ownerId,
+  };
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('participant_id', instance.participantId);
+  val['token'] = instance.token;
+  return val;
+}
