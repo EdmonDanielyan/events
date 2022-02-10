f4e38c378c19168df208cd270c8ddfa09ddd968b
mormih <mormih@gmail.com>
2022-02-10 16:08:45 +0400

    Инвайт NewOnly подписка

--- lib/messenger/listeners/channels_registry.dart
+++ lib/messenger/listeners/channels_registry.dart
@@ -109,7 +109,7 @@ class ChannelsRegistry with Loggable {
     final exists = await channelFunctions.channelExists(inviteUserChannel);
 
     if (!exists) {
-      await _subscribeToChannel(inviteUserChannel);
+      await _subscribeToChannel(inviteUserChannel, startPosition: StartPosition.NewOnly);
     }
   }
 
