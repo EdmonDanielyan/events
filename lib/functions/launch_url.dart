20fd8a1e1f5f82d61816fdcdf1040ce4c0d08b4d
Ибрагим Чекаев <i.chekaev@qsoft.ru>
2022-02-14 14:08:41 +0300

    uri formatter

--- lib/functions/launch_url.dart
+++ lib/functions/launch_url.dart
@@ -2,7 +2,7 @@ import 'package:flutter/services.dart';
 import 'package:ink_mobile/functions/errors.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-Future<void> launchUrl(String url) async {
+Future<void> launchUrl(String url, {bool formatUrl = true}) async {
   try {
     if (!url.contains("http")) {
       final separate = url.split(":");
@@ -12,8 +12,10 @@ Future<void> launchUrl(String url) async {
       }
     }
 
-    await canLaunch(Uri.encodeFull(url))
-        ? await launch(Uri.encodeFull(url))
+    String newUrl = formatUrl ? Uri.encodeFull(url) : url;
+
+    await canLaunch(newUrl)
+        ? await launch(newUrl)
         : showErrorDialog('Не удалось перейти по внешней ссылке');
   } on PlatformException {
     //Unsupported message codecs links
--- lib/screens/auth/components/sign_in_instructions.dart
+++ lib/screens/auth/components/sign_in_instructions.dart
@@ -12,7 +12,8 @@ class SignInInstructions extends StatelessWidget {
     return TextButton(
       onPressed: () {
         launchUrl(
-            'https://portal.irkutskoil.ru/login/Инструкция по регистрации на ИНК портале.pdf');
+            'https://portal.irkutskoil.ru/login/%D0%98%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%86%D0%B8%D1%8F%20%D0%BF%D0%BE%20%D1%80%D0%B5%D0%B3%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D0%B8%20%D0%BD%D0%B0%20%D0%98%D0%9D%D0%9A%20%D0%BF%D0%BE%D1%80%D1%82%D0%B0%D0%BB%D0%B5.pdf',
+            formatUrl: false);
       },
       child: Opacity(
         opacity: 0.6,
