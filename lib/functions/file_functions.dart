2212c07f16fe174c73698cd5597e5ec4a8c5b3cc
Ибрагим Чекаев <i.chekaev@qsoft.ru>
2022-03-10 19:17:56 +0300

    ios build fix

--- lib/functions/file_functions.dart
+++ lib/functions/file_functions.dart
@@ -1,9 +1,5 @@
 import 'dart:io';
-
-import 'package:flutter/material.dart';
-import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:ink_mobile/localization/i18n/i18n.dart';
 
 class FileFunctions {
   Future<File?> getImageFromGallery() async {
@@ -14,23 +10,5 @@ class FileFunctions {
     }
 
     return null;
-  }
-
-  Future<File?> cropImage(File image) async {
-    return await ImageCropper().cropImage(
-      sourcePath: image.path,
-      aspectRatio: CropAspectRatio(ratioX: 1.0, ratioY: 1.0),
-      cropStyle: CropStyle.circle,
-      androidUiSettings: AndroidUiSettings(
-        toolbarTitle: localizationInstance.edit,
-        toolbarColor: Colors.blueGrey[400],
-        statusBarColor: Colors.blueGrey,
-      ),
-      iosUiSettings: IOSUiSettings(
-        title: "",
-        doneButtonTitle: localizationInstance.save,
-        cancelButtonTitle: localizationInstance.cancel,
-      ),
-    );
   }
 }
