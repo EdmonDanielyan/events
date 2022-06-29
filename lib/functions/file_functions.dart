5dfb1e9eaf28260f7eafefa6276cee636c109e69
Ибрагим Чекаев <i.chekaev@qsoft.ru>
2022-06-29 21:42:39 +0300

    getting rid of image_picker

--- lib/functions/file_functions.dart
+++ lib/functions/file_functions.dart
@@ -1,14 +1,1 @@
-import 'dart:io';
-import 'package:image_picker/image_picker.dart';
-
-class FileFunctions {
-  Future<File?> getImageFromGallery() async {
-    final ImagePicker picker = ImagePicker();
-    final image = await picker.pickImage(source: ImageSource.gallery);
-    if (image != null) {
-      return File(image.path);
-    }
-
-    return null;
-  }
-}
+class FileFunctions {}
