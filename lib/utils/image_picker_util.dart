822344ef9e719063352f70297ba1e718d8a33289
@magin.maxim_adict <mm@adictgroup.ru>
2022-12-12 12:47:56 +0300

    optimize image picker

--- lib/utils/image_picker_util.dart
+++ lib/utils/image_picker_util.dart
@@ -1,20 +1,19 @@
 import 'package:image_picker/image_picker.dart';
-import 'package:ink_mobile/utils/permissions_util.dart';
-import 'package:permission_handler/permission_handler.dart';
+
+enum ImagePickerType {
+  gallery,
+  camera
+}
 
 class ImagePickerUtil {
-  PermissionsUtil permissions = PermissionsUtil(PermissionType.photo);
 
-  Future<String?> getFromGallery() async {
-    XFile? pickedFile =
-    await ImagePicker().pickImage(source: ImageSource.gallery);
-    print("PATH OF IMAGE ${pickedFile?.path}");
-    return pickedFile?.path;
-  }
-
-  Future<String?> getFromCamera() async {
-    XFile? pickedFile =
-    await ImagePicker().pickImage(source: ImageSource.camera);
+  Future<String?> getImage(ImagePickerType imagePickerType) async {
+    XFile? pickedFile;
+    if (imagePickerType == ImagePickerType.gallery) {
+      pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+    } else {
+      pickedFile = await ImagePicker().pickImage(source: ImageSource.camera);
+    }
     print("PATH OF IMAGE ${pickedFile?.path}");
     return pickedFile?.path;
   }
