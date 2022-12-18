658f0cc859e7f9aeb7129d09ace8cc37920dcca8
max <max@adictgroup.ru>
2022-12-18 20:37:33 +0800

    поставил URL-ы, работающие из-под VPN

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -2,16 +2,18 @@ import 'package:flutter/foundation.dart';
 
 class UrlsConfig {
   static const prodUrl = "https://portal.irkutskoil.ru/api/v1/";
-  static const debugUrl = "https://test.ink.vpool.qsoft.ru/api/v1";
+  static const debugUrl = "http://mp-test-portal.irkutskoil.ru/api/v1";
+
   static const wsProdUrl = "wss://messenger.irkutskoil.ru:3009";
-  static const wsDebugUrl = "wss://ink.vpool.qsoft.ru";
+  static const wsDebugUrl = "ws://mp-test-portal.irkutskoil.ru:6042";
+
   static const supportUrl = "https://portal.irkutskoil.ru/login/report/";
   static const aboutCompany = "https://irkutskoil.ru/";
   static const pushBaseUrl = "http://ink-portal.ru/";
 
   static Set<String> messengerApiUrls = {
     "https://messenger.irkutskoil.ru:3009/api/v1",
-    "https://ink.vpool.qsoft.ru/api/v1"
+    "http://mp-test-portal.irkutskoil.ru:8080/api/v1"
   };
 
   static String get apiUrl => kDebugMode ? debugUrl : prodUrl;
