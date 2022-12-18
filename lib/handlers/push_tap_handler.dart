587992608a2b89d446b749127afe8f49de05f13c
max <max@adictgroup.ru>
2022-12-18 20:51:12 +0800

    #fix, перепутал пути в навигации

--- lib/handlers/push_tap_handler.dart
+++ lib/handlers/push_tap_handler.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:ink_mobile/constants/urls.dart';
 import 'package:ink_mobile/utils/navigation_methods.dart';
+import 'package:logging/logging.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 class PushTapHandler {
   static BuildContext? currentContext;
   static bool isSubscribed = false;
+  static final logger = new Logger('PUSH-NAV');
 
   static void subscribe() {
     OneSignal.shared.setNotificationOpenedHandler((notification) {
@@ -34,23 +36,34 @@ class PushTapHandler {
           NavigationMethods.openEventDetail(currentContext!, int.parse(pathParts[1]));
         }
         break;
+
+      case "announcements":
+        if (pathParts.length == 1) {
+          NavigationMethods.openAnnouncementsList(currentContext!);
+        } else {
+          NavigationMethods.openAnnouncementDetail(currentContext!, int.parse(pathParts[1]));
+        }
+        break;
+
       case "chat_with":
         if (pathParts.length == 2) {
+          logger.finest('TODO: Open private chat with '+pathParts[1]);
           // TODO: открыть приватный чат по ID собеседника
         }
         break;
-
       case "chat":
         if (pathParts.length == 1) {
+          logger.finest('TODO: Open meddenger');
           // TODO: открыть мессенждер
         } else {
-          // TODO: открыть чат по ID
+          logger.finest('TODO:  Open chat '+pathParts[1]);
         }
         break;
-
-      case "announcements":
+      case "notifications":
+        logger.finest('TODO: Open notifications channel');
         // TODO: открыть чат с уведомлениями
         break;
+
       default:
         break;
     }
