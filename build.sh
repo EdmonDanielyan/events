e2728ca57c201094cd2ea23547fa22f4904ac6f0
Maxim Magin <mm@adictgroup.ru>
2022-12-28 22:19:19 +0300

    working app version

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ platform=$2
 #### Environment values: prod, dev
 #### Platform values: android, ios
 
-flutter clean
-flutter pub upgrade
-flutter packages pub run build_runner build --delete-conflicting-outputs
+#flutter clean
+#flutter pub upgrade
+#flutter packages pub run build_runner build --delete-conflicting-outputs
 
 if [ "$platform" == "android" ]; then
   flutter build apk --release --no-sound-null-safety -t lib/"$env"_main.dart
