3313b45ec5c6cb4afe2b41c92087f342c3342ab3
Maxim Magin <mm@adictgroup.ru>
2023-01-11 10:33:53 +0300

    correct setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#flutter pub get
-#flutter packages pub run build_runner build --delete-conflicting-outputs
-flutter clean
-flutter build ios --no-sound-null-safety -t lib/dev_main.dart
+flutter pub get
+flutter packages pub run build_runner build --delete-conflicting-outputs
+
