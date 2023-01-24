013d0bb62eaff0bd69fd594710b7da7e9b8daaea
Maxim Magin <mm@adictgroup.ru>
2023-01-24 11:43:34 +0300

    Add refresh by scroll

--- lib/messenger/functions/paginator.dart
+++ lib/messenger/functions/paginator.dart
@@ -2,11 +2,13 @@ class Paginator {
   int limit;
   int page;
   bool block;
+  int newMessagesOffset;
 
-  Paginator({required this.limit, this.page = 1, this.block = false});
+  Paginator({required this.limit, this.page = 1, this.block = false, this.newMessagesOffset = 0});
 
   void clear({int? page}) {
     page = page ?? 1;
     block = false;
+    newMessagesOffset = 0;
   }
 }
