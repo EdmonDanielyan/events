a312ddbd711193a7772dffd5b3a4c03b0b087cd5
Maxim Magin <mm@adictgroup.ru>
2023-02-01 18:01:18 +0300

    improve build script

--- build.sh
+++ build.sh
@@ -5,20 +5,25 @@ platform=$2
 #### Environment values: prod, dev, all
 #### Platform values: android, ios
 
+building()
+{
+  build_type=$1
+  environment=$2
+
+  if ["$environment" == all]; then
+    flutter build "$build_type" --release --no-sound-null-safety --flavor dev -t lib/dev_main.dart
+    flutter build "$build_type" --release --no-sound-null-safety --flavor prod -t lib/prod_main.dart
+  fi
+  flutter build "$build_type" --release --no-sound-null-safety --flavor environment -t lib/"$environment"_main.dart
+}
+
 flutter packages pub run build_runner build --delete-conflicting-outputs
 
 if [ "$platform" == "android" ]; then
-  if [ "$env" == "all" ]; then
-    flutter build apk --release --no-sound-null-safety -t lib/dev_main.dart
-    flutter build apk --release --no-sound-null-safety -t lib/prod_main.dart
-  else
-    flutter build apk --release --no-sound-null-safety -t lib/"$env"_main.dart
-  fi
+  building apk "$env"
 elif [ "$platform" == "ios" ]; then
-  if [ "$env" == "all" ]; then
-      flutter build ipa --release --no-sound-null-safety -t lib/dev_main.dart
-      flutter build ipa --release --no-sound-null-safety -t lib/prod_main.dart
-    else
-      flutter build ipa --release --no-sound-null-safety -t lib/"$env"_main.dart
-    fi
+  building ipa "$env"
+elif [ "$platform" == "all" ]; then
+    building apk "$env"
+    building ipa "$env"
 fi
