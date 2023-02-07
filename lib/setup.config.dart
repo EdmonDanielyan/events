3c0745a7295d0230e523076e8056338067f3a995
Maxim Magin <mm@adictgroup.ru>
2023-02-07 12:06:13 +0300

    hide local pushes and turn off notifications button

--- lib/setup.config.dart
+++ lib/setup.config.dart
@@ -188,9 +188,9 @@ Future<_i1.GetIt> $initGetIt(
     environment,
     environmentFilter,
   );
+  final tokenDataInjectorModule = _$TokenDataInjectorModule();
   final testLogFilePathInjector = _$TestLogFilePathInjector();
   final logFilePathInjector = _$LogFilePathInjector();
-  final tokenDataInjectorModule = _$TokenDataInjectorModule();
   gh.factory<_i3.AnnouncementCubit>(() => _i3.AnnouncementCubit());
   gh.factoryParam<_i4.AnnouncementsDetailNetworkRequest, int?, dynamic>((
     announcementId,
@@ -401,29 +401,6 @@ Future<_i1.GetIt> $initGetIt(
   gh.singleton<_i100.StaffMovementsNetworkRequest>(
       _i100.StaffMovementsNetworkRequest());
   gh.factory<String>(
-    () => testLogFilePathInjector.logFile,
-    instanceName: 'logFile',
-    registerFor: {_unitTest},
-  );
-  await gh.factoryAsync<String>(
-    () => logFilePathInjector.logFile,
-    instanceName: 'logFile',
-    registerFor: {
-      _test,
-      _prod,
-      _dev,
-    },
-    preResolve: true,
-  );
-  gh.factory<String>(
-    () => tokenDataInjectorModule.userId,
-    instanceName: 'userId',
-  );
-  gh.factory<String>(
-    () => tokenDataInjectorModule.messengerAuthLogin,
-    instanceName: 'messengerAuthLogin',
-  );
-  gh.factory<String>(
     () => tokenDataInjectorModule.localDatabasePassword,
     instanceName: 'localDatabasePassword',
   );
@@ -438,6 +415,29 @@ Future<_i1.GetIt> $initGetIt(
   gh.factory<String>(
     () => tokenDataInjectorModule.messengerAuthPassword,
     instanceName: 'messengerAuthPassword',
+  );
+  gh.factory<String>(
+    () => tokenDataInjectorModule.messengerAuthLogin,
+    instanceName: 'messengerAuthLogin',
+  );
+  gh.factory<String>(
+    () => tokenDataInjectorModule.userId,
+    instanceName: 'userId',
+  );
+  gh.factory<String>(
+    () => testLogFilePathInjector.logFile,
+    instanceName: 'logFile',
+    registerFor: {_unitTest},
+  );
+  await gh.factoryAsync<String>(
+    () => logFilePathInjector.logFile,
+    instanceName: 'logFile',
+    registerFor: {
+      _test,
+      _prod,
+      _dev,
+    },
+    preResolve: true,
   );
   gh.singleton<_i101.TagsListCubit>(_i101.TagsListCubit());
   gh.singleton<_i102.TagsListNetworkRequest>(_i102.TagsListNetworkRequest());
