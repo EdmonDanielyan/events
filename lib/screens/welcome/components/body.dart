0c964f6a085f3383a04f80ee30a74bd8f9a8007a
Timur I <ti@adictgroup.ru>
2023-03-30 20:22:23 +0800

    правки для ios версии

--- lib/screens/welcome/components/body.dart
+++ lib/screens/welcome/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ink_mobile/components/layout_builder/layout_builder.dart';
@@ -96,7 +97,23 @@ class _BodyState extends State<Body> {
                                 indent: 24,
                                 endIndent: 15,
                               ))),
+                      defaultTargetPlatform == TargetPlatform.iOS?
                       TextButton(
+                        onPressed: () {
+                          launchUrl(
+                              'https://portal.irkutskoil.ru/login/%D0%98%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%86%D0%B8%D1%8F%20%D0%BF%D0%BE%20%D1%80%D0%B5%D0%B3%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D0%B8%20%D0%BD%D0%B0%20%D0%98%D0%9D%D0%9A%20%D0%BF%D0%BE%D1%80%D1%82%D0%B0%D0%BB%D0%B5.pdf',
+                              formatUrl: false);
+                        },
+                        child: Text(
+                          'Инструкция по входу',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: isTablet ? 20 : 16,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      )
+                      :TextButton(
                         onPressed: () {
                           launchUrl(
                               'https://portal.irkutskoil.ru/login/?act=register');
@@ -121,8 +138,10 @@ class _BodyState extends State<Body> {
                               ))),
                     ],
                   ),
-                  const SizedBox(height: 10.0),
-                  SignInInstructions(),
+                  defaultTargetPlatform != TargetPlatform.iOS? Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: SignInInstructions(),
+                  ):SizedBox(),
                   const SizedBox(height: 10.0),
                   SignInInstructions(
                     txt: _strings.techSupport,
