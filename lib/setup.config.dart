866ba46e21cc2785f9ef517b7455fe3f62a93304
Magin Maxim <mm@adictgroup.ru>
2023-05-29 11:19:10 +0300

    v. 1.2.59+159

--- lib/setup.config.dart
+++ lib/setup.config.dart
@@ -193,9 +193,9 @@ Future<_i1.GetIt> $initGetIt(
     environment,
     environmentFilter,
   );
-  final tokenDataInjectorModule = _$TokenDataInjectorModule();
-  final testLogFilePathInjector = _$TestLogFilePathInjector();
   final logFilePathInjector = _$LogFilePathInjector();
+  final testLogFilePathInjector = _$TestLogFilePathInjector();
+  final tokenDataInjectorModule = _$TokenDataInjectorModule();
   gh.factory<_i3.AnnouncementCubit>(() => _i3.AnnouncementCubit());
   gh.factoryParam<_i4.AnnouncementsDetailNetworkRequest, int?, dynamic>((
     announcementId,
@@ -416,35 +416,6 @@ Future<_i1.GetIt> $initGetIt(
       _i103.SingleSectionNetworkRequest(code: code));
   gh.singleton<_i104.StaffMovementsNetworkRequest>(
       _i104.StaffMovementsNetworkRequest());
-  gh.factory<String>(
-    () => tokenDataInjectorModule.deviceVirtualId,
-    instanceName: 'deviceVirtualId',
-  );
-  gh.factory<String>(
-    () => tokenDataInjectorModule.natsToken,
-    instanceName: 'natsToken',
-  );
-  gh.factory<String>(
-    () => tokenDataInjectorModule.messengerAuthPassword,
-    instanceName: 'messengerAuthPassword',
-  );
-  gh.factory<String>(
-    () => tokenDataInjectorModule.messengerAuthLogin,
-    instanceName: 'messengerAuthLogin',
-  );
-  gh.factory<String>(
-    () => tokenDataInjectorModule.userId,
-    instanceName: 'userId',
-  );
-  gh.factory<String>(
-    () => tokenDataInjectorModule.localDatabasePassword,
-    instanceName: 'localDatabasePassword',
-  );
-  gh.factory<String>(
-    () => testLogFilePathInjector.logFile,
-    instanceName: 'logFile',
-    registerFor: {_unitTest},
-  );
   await gh.factoryAsync<String>(
     () => logFilePathInjector.logFile,
     instanceName: 'logFile',
@@ -454,6 +425,35 @@ Future<_i1.GetIt> $initGetIt(
       _dev,
     },
     preResolve: true,
+  );
+  gh.factory<String>(
+    () => testLogFilePathInjector.logFile,
+    instanceName: 'logFile',
+    registerFor: {_unitTest},
+  );
+  gh.factory<String>(
+    () => tokenDataInjectorModule.userId,
+    instanceName: 'userId',
+  );
+  gh.factory<String>(
+    () => tokenDataInjectorModule.messengerAuthLogin,
+    instanceName: 'messengerAuthLogin',
+  );
+  gh.factory<String>(
+    () => tokenDataInjectorModule.messengerAuthPassword,
+    instanceName: 'messengerAuthPassword',
+  );
+  gh.factory<String>(
+    () => tokenDataInjectorModule.localDatabasePassword,
+    instanceName: 'localDatabasePassword',
+  );
+  gh.factory<String>(
+    () => tokenDataInjectorModule.deviceVirtualId,
+    instanceName: 'deviceVirtualId',
+  );
+  gh.factory<String>(
+    () => tokenDataInjectorModule.natsToken,
+    instanceName: 'natsToken',
   );
   gh.singleton<_i105.TagsListCubit>(_i105.TagsListCubit());
   gh.singleton<_i106.TagsListNetworkRequest>(_i106.TagsListNetworkRequest());
